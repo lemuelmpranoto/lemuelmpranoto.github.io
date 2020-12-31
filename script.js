6c9db34b23725f8fc1102ae5632d3dfea11f6c5c
Lemuel Pranoto <lemuelmpranoto@gmail.com>
2020-12-31 14:39:24 +0700

    Moved script to external file

---
+++ script.js
@@ -0,0 +1,74 @@
+TweenMax.from(".logo", 1.6, {
+    delay: 6.4,
+    opacity: 0,
+    y: 30,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".wrapper", 1.6, {
+    delay: 6.5,
+    opacity: 0,
+    y: 30,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".heading", 1.6, {
+    delay: 6.6,
+    opacity: 0,
+    y: 30,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".desc", 1.6, {
+    delay: 6.7,
+    opacity: 0,
+    y: 30,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from("button", 1.6, {
+    delay: 6.8,
+    opacity: 0,
+    y: 30,
+    ease: Expo.easeInOut
+});
+
+// TweenMax.from(".watch", 1.6, {
+//       delay: 6.9,
+//       opacity: 0,
+//       y: 30,
+//       rotation: 90,
+//       ease: Expo.easeInOut
+// });
+
+TweenMax.staggerFrom(".media1", 2, {
+    delay: 7,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.2);
+
+TweenMax.staggerFrom(".media2", 2, {
+    delay: 7,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.2);
+
+TweenMax.staggerFrom(".media3", 2, {
+    delay: 7,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.2);
+
+TweenMax.staggerFrom(".media4", 2, {
+    delay: 7,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.2);
+
+document.querySelector(".right ul li").addEventListener("click", function(){
+      this.classList.toggle("active");
+});
