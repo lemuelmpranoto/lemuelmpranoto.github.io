0ef0c2bcc94c146034bc72d1ad506b24c4a48997
Lemuel Pranoto <lemuelmpranoto@gmail.com>
2021-02-15 20:31:02 +0700

    Updated design

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/* Go back to the top of the page on refresh*/
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
 // TweenMax.from(".logo", 1.6, {
 //     delay: 6.4,
 //     opacity: 0,
@@ -5,12 +10,12 @@
 //     ease: Expo.easeInOut
 // });
 
-TweenMax.from(".content", 1.6, {
-    delay: 6.5,
-    opacity: 0,
-    y: 30,
-    ease: Expo.easeInOut
-});
+// TweenMax.from(".content", 1.6, {
+//     delay: 6.5,
+//     opacity: 0,
+//     y: 30,
+//     ease: Expo.easeInOut
+// });
 
 TweenMax.from(".wrapper", 1.6, {
     delay: 6.5,
@@ -20,6 +25,13 @@ TweenMax.from(".wrapper", 1.6, {
 });
 
 TweenMax.from(".heading", 1.6, {
+    delay: 6.6,
+    opacity: 0,
+    y: 30,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".third-block", 1.6, {
     delay: 6.6,
     opacity: 0,
     y: 30,
@@ -93,3 +105,4 @@ TweenMax.staggerFrom(".media4", 2, {
 document.querySelector(".right ul li").addEventListener("click", function(){
       this.classList.toggle("active");
 });
+
