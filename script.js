da74e6f33c933ebe4ec408bf3c509318e689e715
Lemuel Pranoto <lemuelmpranoto@gmail.com>
2021-02-17 18:24:37 +0700

    Updated animations and removed unnecessary files

--- script.js
+++ script.js
@@ -3,59 +3,10 @@ window.onbeforeunload = function () {
     window.scrollTo(0, 0);
   }
 
-// TweenMax.from(".logo", 1.6, {
-//     delay: 6.4,
-//     opacity: 0,
-//     y: 30,
-//     ease: Expo.easeInOut
-// });
-
-// TweenMax.from(".content", 1.6, {
-//     delay: 6.5,
-//     opacity: 0,
-//     y: 30,
-//     ease: Expo.easeInOut
-// });
-
-TweenMax.from(".wrapper", 1.6, {
-    delay: 6.5,
+TweenMax.from(".wrapper", 1.8, {
+    delay: 6.9,
     opacity: 0,
-    y: 30,
-    ease: Expo.easeInOut
-});
-
-TweenMax.from(".heading", 1.6, {
-    delay: 6.6,
-    opacity: 0,
-    y: 30,
-    ease: Expo.easeInOut
-});
-
-TweenMax.from(".third-block", 1.6, {
-    delay: 6.6,
-    opacity: 0,
-    y: 30,
-    ease: Expo.easeInOut
-});
-
-TweenMax.from(".desc", 1.6, {
-    delay: 6.7,
-    opacity: 0,
-    y: 30,
-    ease: Expo.easeInOut
-});
-
-// TweenMax.from(".aboutme", 1.6, {
-//     delay: 6.7,
-//     opacity: 0,
-//     y: 30,
-//     ease: Expo.easeInOut
-// });
-
-TweenMax.from("button", 1.6, {
-    delay: 6.8,
-    opacity: 0,
-    y: 30,
+    y: -50,
     ease: Expo.easeInOut
 });
 
@@ -66,6 +17,92 @@ TweenMax.from("loaderafter", 1.6, {
     ease: Expo.easeInOut
 });
 
+TweenMax.from(".third-block", 1.6, {
+    delay: 6.2,
+    opacity: 0,
+    // y: 30,
+    // ease: Expo.easeInOut
+});
+
+TweenMax.from(".border", 3.2, {
+    delay: 7.7,
+    opacity: 0,
+    x: -50,
+    ease: Expo.easeInOut
+});
+
+// TweenMax.from(".content", 1.6, {
+//     delay: 6.5,
+//     opacity: 0,
+//     y: 30,
+//     ease: Expo.easeInOut
+// });
+
+TweenMax.staggerFrom(".media1", 2.3, {
+    delay: 7.3,
+    opacity: 0,
+    x: 40,
+    ease: Expo.easeInOut
+}, 0.2);
+
+TweenMax.staggerFrom(".media2", 2.3, {
+    delay: 7.3,
+    opacity: 0,
+    x: 40,
+    ease: Expo.easeInOut
+}, 0.2);
+
+TweenMax.staggerFrom(".media3", 2.3, {
+    delay: 7.3,
+    opacity: 0,
+    x: 40,
+    ease: Expo.easeInOut
+}, 0.2);
+
+TweenMax.staggerFrom(".media4", 2.3, {
+    delay: 7.3,
+    opacity: 0,
+    x: 40,
+    ease: Expo.easeInOut
+}, 0.2);
+
+document.querySelector(".right ul li").addEventListener("click", function(){
+      this.classList.toggle("active");
+});
+
+
+
+
+
+
+// TweenMax.from(".heading", 1.6, {
+//     delay: 6.6,
+//     opacity: 0,
+//     y: 30,
+//     ease: Expo.easeInOut
+// });
+
+// TweenMax.from(".desc", 1.6, {
+//     delay: 6.7,
+//     opacity: 0,
+//     y: 30,
+//     ease: Expo.easeInOut
+// });
+
+// TweenMax.from(".aboutme", 1.6, {
+//     delay: 6.7,
+//     opacity: 0,
+//     y: 30,
+//     ease: Expo.easeInOut
+// });
+
+// TweenMax.from("button", 1.6, {
+//     delay: 6.8,
+//     opacity: 0,
+//     y: 30,
+//     ease: Expo.easeInOut
+// });
+
 // TweenMax.from(".watch", 1.6, {
 //       delay: 6.9,
 //       opacity: 0,
@@ -74,35 +111,9 @@ TweenMax.from("loaderafter", 1.6, {
 //       ease: Expo.easeInOut
 // });
 
-TweenMax.staggerFrom(".media1", 2, {
-    delay: 7,
-    opacity: 0,
-    y: 40,
-    ease: Expo.easeInOut
-}, 0.2);
-
-TweenMax.staggerFrom(".media2", 2, {
-    delay: 7,
-    opacity: 0,
-    y: 40,
-    ease: Expo.easeInOut
-}, 0.2);
-
-TweenMax.staggerFrom(".media3", 2, {
-    delay: 7,
-    opacity: 0,
-    y: 40,
-    ease: Expo.easeInOut
-}, 0.2);
-
-TweenMax.staggerFrom(".media4", 2, {
-    delay: 7,
-    opacity: 0,
-    y: 40,
-    ease: Expo.easeInOut
-}, 0.2);
-
-document.querySelector(".right ul li").addEventListener("click", function(){
-      this.classList.toggle("active");
-});
-
+// TweenMax.from(".logo", 1.6, {
+//     delay: 6.4,
+//     opacity: 0,
+//     y: 30,
+//     ease: Expo.easeInOut
+// });
