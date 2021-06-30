2bb4932293cad659b26e5fe3e216aa6c0a0e5145
Lemuel Pranoto <lemuelmpranoto@gmail.com>
2021-06-30 17:58:30 +0700

    updated socials overlay panel

--- v2/script.js
+++ v2/script.js
@@ -157,10 +157,10 @@ window.onload = function() {
 }
 
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("overlay-socials").style.width = "100%";
 }
 function closeNav() {
-    document.getElementById("myNav").style.width = "";
+    document.getElementById("overlay-socials").style.width = "";
 }
 
 document.addEventListener('mousemove', function(e) {
